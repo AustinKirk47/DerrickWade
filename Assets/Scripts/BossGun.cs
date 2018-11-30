7fa6400551dddb9df08464e226cd653e47cbc017
TNTrevor <43008167+TNTrevor@users.noreply.github.com>
2018-11-29 20:50:35 -0600

    boss pertial completion
    boss stage

---
+++ Assets/Scripts/BossGun.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGun : MonoBehaviour
+{
+
+    public float ProjectileSpeed = 10;
+    public float FireRate = 1;
+    public float FollowDistance = 10;
+    public GameObject Projectile;
+    public GameObject[] Loot;
+
+    private Rigidbody2D body;
+    private GameObject player;
+
+    private float epsilon = 5f;
+    private float FireAllowance = 0;
+    private int LastSecond = 0;
+
+    private float SpawnTime;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.freezeRotation = true;
+
+        player = GameObject.FindWithTag("Player");
+
+        SpawnTime = Time.time;
+    }
+
+    void FixedUpdate()
+    {
+        if (Time.time - SpawnTime < 2)
+        {
+            return;
+        }
+        AimAtPlayer();
+
+        int second = (int)Time.time;
+        if (second != LastSecond)
+        {
+            LastSecond = second;
+            FireAllowance += FireRate;
+        }
+
+        if (FireAllowance > 0 && Random.Range(0, 50) < 1)
+        {
+            Fire();
+        }
+    }
+
+    public void OnDeath()
+    {
+        if (Random.Range(0f, 1f) < 0.75f && Loot.Length > 0)
+        {
+            int i = Random.Range(0, Loot.Length);
+            GameObject loot = Instantiate(Loot[i]);
+            loot.transform.position = transform.position;
+            loot.GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle * 0.5f;
+            loot.GetComponent<Rigidbody2D>().angularVelocity = Random.value * 20;
+        }
+    }
+
+    void Fire()
+    {
+        Vector2 direction = new Vector2(-Mathf.Sin(Mathf.Deg2Rad * body.rotation), Mathf.Cos(Mathf.Deg2Rad * body.rotation));
+
+        GameObject p = Instantiate(Projectile);
+        p.transform.position = transform.position + new Vector3(direction.x, direction.y, 0) * 1.5f;
+        p.GetComponent<Rigidbody2D>().velocity = direction * ProjectileSpeed;
+
+        Destroy(p, 5);
+        FireAllowance--;
+    }
+    void AimAtPlayer()
+    {
+        Vector2 playerPos = player.transform.position;
+        float angle = Mathf.Atan2(playerPos.y - transform.position.y, playerPos.x - transform.position.x) * Mathf.Rad2Deg - 90;
+
+        body.rotation = angle;
+    }
+}
---
+++ Assets/Scripts/BossLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLevel : MonoBehaviour
+{
+
+    public GameObject Enemy;
+    public GameObject SpawnEffect;
+    public Transform[] SpawnPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    
+
+    void SpawnEnemy(Vector3 position, Quaternion rotation)
+    {
+        GameObject effect = Instantiate(SpawnEffect, position, Quaternion.identity);
+        Instantiate(Enemy, position, rotation);
+        Destroy(effect, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

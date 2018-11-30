43bda6c136438697db6e299b6c6814207d53da60
jacobbrunson <jacob@brunson.me>
2018-11-29 22:20:57 -0600

    fixed boss turrets

--- Assets/Scripts/BossGun.cs
+++ Assets/Scripts/BossGun.cs
@@ -68,7 +68,7 @@ public class BossGun : MonoBehaviour
         Vector2 direction = new Vector2(-Mathf.Sin(Mathf.Deg2Rad * body.rotation), Mathf.Cos(Mathf.Deg2Rad * body.rotation));
 
         GameObject p = Instantiate(Projectile);
-        p.transform.position = transform.position + new Vector3(direction.x, direction.y, 0) * 1.5f;
+        p.transform.position = transform.position + new Vector3(direction.x, direction.y, 0) * 3.5f;
         p.GetComponent<Rigidbody2D>().velocity = direction * ProjectileSpeed;
 
         Destroy(p, 5);
